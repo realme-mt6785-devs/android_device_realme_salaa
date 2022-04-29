5f346501759536f3bf4b1d20cbbcd9805dcbfb3c
Roshan Pius <rpius@google.com>
2022-04-29 19:42:03 +0100

    salaa: rro_overlays: Add WifiOverlay
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    
    Change-Id: Iab6ec7ff4a24e7e92f97231f139e31cb57e7501f

--- device.mk
+++ device.mk
@@ -223,6 +223,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGES += \
+    WifiOverlay
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
