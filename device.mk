df522f01a8886f66b9802936e900074d906a588b
Weilun Du <wdu@google.com>
2022-05-09 17:56:49 +0100

    salaa: rro_overlays: Add TetheringOverlay
    BUG: 163800715
    
    Change-Id: Ie7a61029924eb8e12b6e1a583951612ddcf351fe

--- device.mk
+++ device.mk
@@ -224,7 +224,8 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
-    WifiOverlay
+    WifiOverlay \
+    TetheringConfigOverlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
