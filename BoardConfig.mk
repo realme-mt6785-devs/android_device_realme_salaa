5720c2379fb6cf7d860aba5fa906d55b28331b76
Aayush Gupta <aayushgupta219@gmail.com>
2022-06-19 20:08:41 +0100

    saala: Inherit sepolicy for mediatek devices
    Change-Id: Id615f35a0d896a6fe8646f9755a06781ec189715

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -126,6 +126,9 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # SPL
 VENDOR_SECURITY_PATCH := 2023-07-05
 
+# SELinux
+include device/mediatek/sepolicy_vndr/SEPolicy.mk
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
