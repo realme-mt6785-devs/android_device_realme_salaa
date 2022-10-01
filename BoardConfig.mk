ae5fa2dafc88389821f39b4a2af0af370a3e3766
Sal Savage <salsavage@google.com>
2022-10-01 13:57:30 +0100

    salaa: Set default the Bluetooth class of device
    The class of device is a string with a list of uint8t values:
    90,2,12
    
    The meaning is as follows:
    {Service Field, Major class, Minor class}
    
    Service Field: 0x5A -> 90
        Bit 17: Networking
        Bit 19: Capturing
        Bit 20: Object Transfer
        Bit 22: Telephony
    MAJOR_CLASS: 0x02 -> 2 (Phone)
    MINOR_CLASS: 0x0C -> 12 (Smart Phone)
    
    Bug: 217452259
    Test: make -j; -- check that bluetooth.device.class_of_device is correct
    Change-Id: Ia3d8f63e422be8b44519f83f8fb01d2edbfbed71

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,6 +107,7 @@ TARGET_BOARD_PLATFORM := mt6785
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
