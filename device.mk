715799c38a5b0b7ec00b64cdc4fc319d544846b1
bengris32 <bengris32@protonmail.ch>
2022-12-13 18:53:09 +0000

    salaa: Build required Camera interfaces
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I0955c0b711cacae4ec6182b6608635257d35903d

--- device.mk
+++ device.mk
@@ -39,6 +39,20 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth@1.1.vendor
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.common@1.0.vendor \
+    android.hardware.camera.device@3.2.vendor \
+    android.hardware.camera.device@1.0.vendor \
+    android.hardware.camera.device@3.3.vendor \
+    android.hardware.camera.device@3.4.vendor \
+    android.hardware.camera.device@3.5.vendor \
+    android.hardware.camera.device@3.6.vendor \
+    android.hardware.camera.provider@2.4.vendor \
+    android.hardware.camera.provider@2.5.vendor \
+    android.hardware.camera.provider@2.6.vendor \
+    libcamera2ndk_vendor
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
