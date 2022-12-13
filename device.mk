5c0fe844945a372c1e5ac7191f53146b25bb3217
bengris32 <bengris32@protonmail.ch>
2022-12-13 19:50:06 +0000

    salaa: Build required keymaster interfaces/libs
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: Ia670cbdf214f2190c447857e582a9795e8986d9b

--- device.mk
+++ device.mk
@@ -93,6 +93,15 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.0.vendor \
+    libkeymaster4.vendor:64 \
+    libkeymaster4support.vendor:64 \
+    libsoft_attestation_cert.vendor:64 \
+    libkeystore-wifi-hidl \
+    libkeystore-engine-wifi-hidl
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light-service.salaa
