c092f1dbbfcfabc3a48396faa7dca9dd086c39cc
LuK1337 <priv.luk@gmail.com>
2022-12-13 19:13:56 +0000

    salaa: Build libhidltransport/libhwbinder
    * Needed for old blobs
    
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172

--- device.mk
+++ device.mk
@@ -86,6 +86,13 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light-service.salaa
