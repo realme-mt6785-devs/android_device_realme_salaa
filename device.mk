b147de9656b03e1139b02fb3bf8fb7aa77e1695f
bengris32 <bengris32@protonmail.ch>
2022-12-13 19:50:51 +0000

    salaa: Build required Radio interfaces
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I3c4a9e4a04feba88f83256a3fbe6b8e586bcd529

--- device.mk
+++ device.mk
@@ -118,6 +118,20 @@ PRODUCT_PACKAGES += \
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# Radio
+PRODUCT_PACKAGES += \
+    android.hardware.radio.config@1.0.vendor \
+    android.hardware.radio.config@1.1.vendor \
+    android.hardware.radio.config@1.2.vendor \
+    android.hardware.radio.config@1.3.vendor \
+    android.hardware.radio@1.0.vendor \
+    android.hardware.radio@1.1.vendor \
+    android.hardware.radio@1.2.vendor \
+    android.hardware.radio@1.3.vendor \
+    android.hardware.radio@1.4.vendor \
+    android.hardware.radio@1.5.vendor \
+    android.hardware.radio@1.6.vendor
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     factory_init.connectivity.common.rc \
