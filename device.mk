7dd8530c6d2f422991e469f4c23ebda40683953d
bengris32 <bengris32@protonmail.ch>
2022-12-13 19:58:46 +0000

    salaa: Build default Composer 2.2 service
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: Ieedbbd72fd16ba97a574a935b43560ec3e942261

--- device.mk
+++ device.mk
@@ -65,6 +65,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0.vendor \
     android.hardware.graphics.allocator@3.0.vendor \
     android.hardware.graphics.allocator@4.0.vendor \
+    android.hardware.graphics.composer@2.2-service \
     libdrm.vendor
 
 # fastbootd
