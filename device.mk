c86e7bbebcd68c7237fd5bd7429a32cce7b0b277
bengris32 <bengris32@protonmail.ch>
2022-12-13 18:55:43 +0000

    salaa: Build default Gatekeeper service/impl
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I2630cfa2c3308a7e8a3d41e22c20b198a6f40518

--- device.mk
+++ device.mk
@@ -64,6 +64,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-service \
+    android.hardware.gatekeeper@1.0-impl
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
