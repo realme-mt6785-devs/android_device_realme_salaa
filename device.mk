0316921ce672a7615eb095c646fb09d6cc4a9d23
bengris32 <bengris32@protonmail.ch>
2022-12-14 16:58:43 +0000

    salaa: Inherit 4GB Dalvik heap configuration
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I11fe2eab53dcaf1052528318c41a9e661502f6eb

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Dalvik VM Configuration
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
