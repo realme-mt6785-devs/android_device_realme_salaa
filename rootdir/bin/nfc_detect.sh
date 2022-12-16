7dbfd7fd0d82ede06429de6449a64b08f70c1f09
bengris32 <bengris32@protonmail.ch>
2022-12-16 20:58:24 +0000

    salaa: Switch over to vendor SKU manifests
    * Oplus thought it would be a great idea to set up ODM SKUs for
      ss and dsds variants, except that salaa only has a dsds variant...
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I6e34ed4e8859fe970307314127d811a0f7376c81

--- rootdir/bin/nfc_detect.sh
+++ rootdir/bin/nfc_detect.sh
@@ -7,6 +7,6 @@
 
 if [ -f /proc/oplus_nfc/chipset ]; then
     if [ $(cat /proc/oplus_nfc/chipset) != "NULL" ]; then
-        setprop ro.boot.product.hardware.sku nfc
+        setprop ro.boot.product.vendor.sku nfc
     fi
 fi
