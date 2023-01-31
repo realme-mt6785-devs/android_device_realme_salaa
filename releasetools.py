29b760dd57d7cb596dd6be8f2242e694df3ad262
XiNGRZ <chenxingyu92@gmail.com>
2023-01-31 17:40:32 +0000

    salaa: Add dtbo image to the output zip if present
    Change-Id: Ie405c40081df3cba9ec5ae2c5e27582c8b4d8769

--- releasetools.py
+++ releasetools.py
@@ -39,6 +39,7 @@ def AddImage(info, basename, dest):
 
 def OTA_InstallEnd(info):
   info.script.Print("Patching firmware images...")
+  AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo")
   AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
   AddImage(info, "vbmeta_system.img", "/dev/block/by-name/vbmeta_system")
   AddImage(info, "vbmeta_vendor.img", "/dev/block/by-name/vbmeta_vendor")
