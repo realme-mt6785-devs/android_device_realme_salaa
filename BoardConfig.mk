fe98378c0831380034bb7db3c7cecc3bb840b366
bengris32 <bengris32@protonmail.ch>
2023-02-02 20:27:28 +0000

    salaa: Set `BOARD_HAS_MTK_HARDWARE` to true
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I288c7844fd58339af97f6a637b42623910e411d4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,6 +106,7 @@ TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 
 # Platform
 TARGET_BOARD_PLATFORM := mt6785
+BOARD_HAS_MTK_HARDWARE := true
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
