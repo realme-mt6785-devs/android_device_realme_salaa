efa3de920a266a7fec3c6e77e748feb7221db3f9
bengris32 <bengris32@protonmail.ch>
2023-05-28 18:21:43 +0100

    salaa: extract-files: Use wildcard match instead of fallthrough case
    * Convert the fallthrough case for patching 32-bit PQ impl into
      a single case by using a wildcard to match for "lib*" so both
      lib and lib64 will be matched.
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I569c624bd2a476872a94414ced92e0f0c81da95a

--- extract-files.sh
+++ extract-files.sh
@@ -57,9 +57,7 @@ function blob_fixup {
         vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so)
             grep -q "libcamera_metadata_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcamera_metadata_shim.so" "${2}"
             ;;
-        vendor/lib/hw/vendor.mediatek.hardware.pq@2.13-impl.so)
-            ;&
-        vendor/lib64/hw/vendor.mediatek.hardware.pq@2.13-impl.so)
+        vendor/lib*/hw/vendor.mediatek.hardware.pq@2.13-impl.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
         vendor/lib*/libmtkcam_stdutils.so)
