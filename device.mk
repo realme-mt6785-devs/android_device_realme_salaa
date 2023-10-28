e8d56ccae7d6a85e497d24a2f5a0431aca94621f
Debayan Kar <kardebayan3@gmail.com>
2023-10-28 21:42:40 +0530

    salaa: Switch to source built Power AIDL

--- device.mk
+++ device.mk
@@ -286,6 +286,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power-service-mediatek \
     android.hardware.power-V2-ndk_platform.vendor \
     android.hardware.power@1.0.vendor \
     android.hardware.power@1.1.vendor \
