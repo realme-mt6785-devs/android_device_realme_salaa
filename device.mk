42421b6f3a36e4cf039ed63b6da5bb93bfd2bc15
Debayan Kar <kardebayan3@gmail.com>
2023-11-15 18:00:38 +0530

    salaa: Build android.hardware.drm@1.4

--- device.mk
+++ device.mk
@@ -101,10 +101,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
 
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0.vendor \
-    android.hardware.drm@1.1.vendor \
-    android.hardware.drm@1.2.vendor \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.4.vendor
 
 # Display
 PRODUCT_PACKAGES += \
