7cd137e54396d91b363865c3016fbb5530df62cd
R0rt1z2 <me@r0rt1z2.com>
2023-11-15 19:18:44 +0530

    salaa: Add shim for libsink
    * Since commit eb57617 [1], the legacy callback interfaces in the Android
      AudioTrack system have been deprecated and replaced by a newer callback
      interface. Consequently, the user parameter, which was previously part
      of the constructor, has been incorporated into this new interface.
    
    * To address the resulting build issues, update the function signatures
      to match the new methods. Although AOSP provides a wrapper [2] for this
      purpose, it's declared within an anonymous namespace, effectively making
      it private to its own translation unit. To work around this limitation,
      provide a local copy of the wrapper instead.
    
    * Also format the shim with `clang-format`.
    
    - [1]: https://cs.android.com/android/_/android/platform/frameworks/av/+/eb57617e55adf5eb5d554d1ad6a8d1c4b4e3740b
    - [2]: https://cs.android.com/android/platform/superproject/main/+/main:frameworks/av/media/libaudioclient/AudioTrack.cpp;l=289;bpv=1

--- device.mk
+++ device.mk
@@ -168,6 +168,9 @@ PRODUCT_BOOT_JARS += \
     oplus-framework \
     oplus-framework-telephony
 
+PRODUCT_PACKAGES += \
+    libshim_vtservice
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
 
